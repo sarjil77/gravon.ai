4b167ba02a975f208db949b005d2b09cd1c77862
sarjil <sarjil2893@gmail.com>
2026-02-12 17:04:24 +0000

    resolving error

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,11 +12,11 @@ export default defineConfig(({ mode }) => ({
     },
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "http://127.0.0.1:8000",
         changeOrigin: true,
       },
       "/wa": {
-        target: "http://localhost:3001",
+        target: "http://127.0.0.1:3001",
         changeOrigin: true,
       },
     },
